e57d01312f8b9c8e8e42f20297c0023874c87578
budihan <b.s.han@student.tudelft.nl>
2022-06-11 16:25:13 +0200

    add milestone lines

--- client_fake.js
+++ client_fake.js
@@ -4,8 +4,8 @@ var task_completed = false;
 
 // progress bar vars
 var curr_task = 0;
-var total_tasks = 60;
-var interval_milestone = 15;
+var total_tasks = 10;
+var interval_milestone = 2;
 
 var start_time = 0;
 var last_time = 0;
@@ -128,6 +128,11 @@ var increment_progress = function() {
     var elem = document.getElementById("myBar");   
   
     if (curr_task < total_tasks) {
+        if((curr_task % total_tasks) == 0) {
+            console.log("MILESTONE TRIGGERED");
+
+            // update link
+        }
       curr_task+=1;
       console.log(curr_task);
       elem.style.width = (curr_task/total_tasks) * 100 + '%';
